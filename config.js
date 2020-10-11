7fb12a60b2cb6f34a9725ded536978d564356bac
David Ma <david.ma@petermac.org>
2020-10-11 22:32:14 +1100

    Add published files to config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
-exports.config = {}
+exports.config = {
+    publish: {
+        dist: [
+            "css/cas13.css",
+            "css/main.css",
+            "js/bionode-seq.min.js",
+            "js/cas13.js",
+            "js/showdown.min.js",
+            "js/vendor.min.js",
+            "favicon.ico",
+            "index.html"
+        ]
+    }
+}
