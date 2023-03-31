45288b04554aa4d3247413b163f94572b3712f4d
David Ma <hello@david-ma.net>
2023-03-31 11:28:43 +1100

    Update docs folder

--- docs/js/cas13.js
+++ docs/js/cas13.js
@@ -5,33 +5,36 @@ const seq = require('bionode-seq');
 let goodScore, badScore, bannedSequences;
 refreshSequences();
 function refreshSequences() {
-    goodScore = `${$('#goodString').val()}`.replaceAll(" ", "").split('');
-    badScore = `${$('#badString').val()}`.replaceAll(" ", "").split('');
-    bannedSequences = `${$('#bannedSeq').val()}`.replaceAll(" ", "").split(',').map(s => s.trim());
+    goodScore = `${$('#goodString').val()}`.replaceAll(' ', '').split('');
+    badScore = `${$('#badString').val()}`.replaceAll(' ', '').split('');
+    bannedSequences = `${$('#bannedSeq').val()}`
+        .replaceAll(' ', '')
+        .split(',')
+        .map((s) => s.trim());
     $('#goodString').val(goodScore.join(''));
     $('#badString').val(badScore.join(''));
     $('#bannedSeq').val(bannedSequences.join(', '));
 }
 function filterBannedSequences(seq) {
-    return !bannedSequences.some(banned => seq.includes(banned));
+    return !bannedSequences.some((banned) => seq.includes(banned));
 }
 const nucleicAcidNotation = {
-    'A': 'A',
-    'C': 'C',
-    'G': 'G',
-    'T': 'T',
-    'U': 'U',
-    'R': 'AG',
-    'Y': 'CT',
-    'S': 'GC',
-    'W': 'AT',
-    'K': 'GT',
-    'M': 'AC',
-    'B': 'CGT',
-    'D': 'AGT',
-    'H': 'ACT',
-    'V': 'ACG',
-    'N': '',
+    A: 'A',
+    C: 'C',
+    G: 'G',
+    T: 'T',
+    U: 'U',
+    R: 'AG',
+    Y: 'CT',
+    S: 'GC',
+    W: 'AT',
+    K: 'GT',
+    M: 'AC',
+    B: 'CGT',
+    D: 'AGT',
+    H: 'ACT',
+    V: 'ACG',
+    N: '',
     // 'N': 'ACGT',
 };
 globalThis.score = score;
@@ -43,12 +46,12 @@ function score(seq) {
 }
 // Colors generated using https://www.sessions.edu/color-calculator/
 const colors = {
-    'green': '#87cc7a',
-    'yellow': '#f2d666',
-    'red': '#cc7a87',
-    'purple': '#be7acc',
-    'blue': '#6682f2',
-    'white': '#ffffff'
+    green: '#87cc7a',
+    yellow: '#f2d666',
+    red: '#cc7a87',
+    purple: '#be7acc',
+    blue: '#6682f2',
+    white: '#ffffff',
 };
 function markup(seq) {
     const result = [];
@@ -60,7 +63,7 @@ function markup(seq) {
             color = colors.green;
             score = 10;
             if (i < 4) {
-                score = 20;
+                score = 60;
             }
             if (nucleicAcidNotation[goodScore[i]].length > 1) {
                 color = colors.yellow;
@@ -69,9 +72,15 @@ function markup(seq) {
         }
         if (nucleicAcidNotation[badScore[i]].includes(char)) {
             color = colors.red;
-            score = -10;
-            if (i > 4) {
-                score = -5;
+            score = -5;
+            if (i < 2) {
+                score = -60;
+            }
+            else if (i < 3) {
+                score = -50;
+            }
+            else if (i < 4) {
+                score = -40;
             }
         }
         result.push({
@@ -114,8 +123,10 @@ function clearResults() {
 }
 globalThis.getOptions = getOptions;
 function getOptions() {
-    const data = $('form').serializeArray()
+    const data = $('form')
+        .serializeArray()
         .reduce((result, val) => {
+        ;
         result[val.name] = val.value;
         return result;
     }, {});
@@ -123,7 +134,7 @@ function getOptions() {
         strandsShown: data.strandsShown,
         format: data.format,
         lines: data.lines,
-        separator: data.separator
+        separator: data.separator,
     };
     return options;
 }
@@ -149,7 +160,9 @@ function submitSequence(sorted = false) {
     let pos = 0;
     if ($('#spacerLength').val() === '')
         spacerLength = 30;
-    if ((!Number.isInteger(spacerLength) && $('#spacerLength').val() !== '') || spacerLength < 1 || parseInt(`${$('#spacerLength').val()}`) === 0) {
+    if ((!Number.isInteger(spacerLength) && $('#spacerLength').val() !== '') ||
+        spacerLength < 1 ||
+        parseInt(`${$('#spacerLength').val()}`) === 0) {
         const message = "Spacer Length must be an integer 1 or greater, setting 'spacerLength' to 1.";
         errors.push(message);
         spacerLength = 1;
@@ -173,7 +186,7 @@ function submitSequence(sorted = false) {
     // Save sequences to #output for charting purposes
     d3.select('#output').datum({
         forwardsequence: forwardsequence,
-        reversesequence: reversesequence
+        reversesequence: reversesequence,
     });
     // drawChart()
     // Apply options
@@ -197,7 +210,8 @@ function submitSequence(sorted = false) {
             .filter(filterBannedSequences)
             .sort((a, b) => {
             return score(b) - score(a);
-        }).map(d => {
+        })
+            .map((d) => {
             return `${d} ${score(d)}`;
         });
         // reversesequence = reversesequence.sort((a, b) => {
@@ -268,23 +282,24 @@ function submitSequence(sorted = false) {
 }
 function printToTable(forwardsequence) {
     // console.log(forwardsequence)
-    d3.select("#outputTable tbody").selectAll("tr")
+    d3.select('#outputTable tbody')
+        .selectAll('tr')
         .data(forwardsequence)
         .enter()
-        .append("tr")
+        .append('tr')
         .each((data, i, array) => {
         var tr = d3.select(array[i]);
-        var d = data.replace(" - ", " ").split(" ");
-        tr.append("td").text(d[0]);
-        tr.append("td")
-            .selectAll("mark")
+        var d = data.replace(' - ', ' ').split(' ');
+        tr.append('td').text(d[0]);
+        tr.append('td')
+            .selectAll('mark')
             .data(markup(d[1]))
             .enter()
-            .append("mark")
-            .attr("title", d => `Score: ${d.score}`)
-            .style('background', d => d.color)
-            .text(d => d.base);
-        tr.append("td").text(d[2]);
+            .append('mark')
+            .attr('title', (d) => `Score: ${d.score}`)
+            .style('background', (d) => d.color)
+            .text((d) => d.base);
+        tr.append('td').text(d[2]);
     });
 }
 globalThis.updateData = updateData;
@@ -295,7 +310,7 @@ globalThis.countGCcontent = countGCcontent;
 function countGCcontent(sequence) {
     const total = sequence.length;
     let gc = 0;
-    sequence.split('').forEach(char => {
+    sequence.split('').forEach((char) => {
         if (char === 'c' || char === 'C' || char === 'g' || char === 'G')
             gc++;
     });
@@ -330,7 +345,9 @@ if (window.location.hash === '#advanced') {
 }
 function drawChart() {
     console.log('Drawing chart');
-    const rawData = d3.select('#output').datum();
+    const rawData = d3
+        .select('#output')
+        .datum();
     let data = [];
     data = data.concat(rawData.forwardsequence.map((d, i) => {
         return {
@@ -338,7 +355,7 @@ function drawChart() {
             label: `gRNA_${i + 1}_F`,
             seq: d,
             gc: countGCcontent(d),
-            type: 'forward'
+            type: 'forward',
         };
     }));
     data = data.concat(rawData.reversesequence.map((d, i) => {
@@ -347,7 +364,7 @@ function drawChart() {
             label: `gRNA_${i + 1}_R`,
             seq: d,
             gc: countGCcontent(d),
-            type: 'reverse'
+            type: 'reverse',
         };
     }));
     // set the dimensions and margins of the graph
@@ -356,7 +373,8 @@ function drawChart() {
     const height = 600 - margin.top - margin.bottom;
     d3.select('#chart g').remove();
     // append the svg object to the body of the page
-    const svg = d3.select('#chart')
+    const svg = d3
+        .select('#chart')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
@@ -370,38 +388,49 @@ function drawChart() {
         .attr('width', width)
         .style('fill', '#EBEBEB');
     // Add X axis
-    const x = d3.scaleLinear()
+    const x = d3
+        .scaleLinear()
         .domain([-10, 10 + rawData.forwardsequence.length])
         .range([0, width]);
-    svg.append('g')
+    svg
+        .append('g')
         .attr('transform', 'translate(0,' + height + ')')
-        .call(d3.axisBottom(x).tickSize(-height * 1.3).ticks(10))
-        .select('.domain').remove();
+        .call(d3
+        .axisBottom(x)
+        .tickSize(-height * 1.3)
+        .ticks(10))
+        .select('.domain')
+        .remove();
     // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height, 0])
-        .nice();
-    svg.append('g')
-        .call(d3.axisLeft(y).tickSize(-width * 1.3).ticks(7))
-        .select('.domain').remove();
+    const y = d3.scaleLinear().domain([0, 100]).range([height, 0]).nice();
+    svg
+        .append('g')
+        .call(d3
+        .axisLeft(y)
+        .tickSize(-width * 1.3)
+        .ticks(7))
+        .select('.domain')
+        .remove();
     // Customization
     svg.selectAll('.tick line').attr('stroke', 'white');
     // Add X axis label:
-    svg.append('text')
+    svg
+        .append('text')
         .attr('text-anchor', 'end')
         .attr('x', width / 2 + margin.left)
         .attr('y', height + margin.top + 20)
         .text('gRNA position');
     // Y axis label:
-    svg.append('text')
+    svg
+        .append('text')
         .attr('text-anchor', 'end')
         .attr('transform', 'rotate(-90)')
         .attr('y', -margin.left + 20)
         .attr('x', -margin.top - height / 2 + 20)
         .text('GC content %');
     // Color scale: give me a specie name, I return a color
-    const color = d3.scaleOrdinal()
+    const color = d3
+        .scaleOrdinal()
         .domain(['forward', 'reverse', 'average'])
         .range(['#F8766D', '#00BA38', '#619CFF']);
     let tooltip = null;
@@ -411,17 +440,24 @@ function drawChart() {
     let sequence = null;
     let gcContent = null;
     // Add dots
-    svg.append('g')
+    svg
+        .append('g')
         .selectAll('dot')
         .data(data)
         .enter()
         .append('circle')
-        .attr('cx', function (d) { return x(d.x); })
-        .attr('cy', function (d) { return y(d.gc); })
+        .attr('cx', function (d) {
+        return x(d.x);
+    })
+        .attr('cy', function (d) {
+        return y(d.gc);
+    })
         .attr('r', 4)
         .style('opacity', 0.5)
-        .style('fill', function (d) { return color(d.type); })
-        .on('mouseover', d => {
+        .style('fill', function (d) {
+        return color(d.type);
+    })
+        .on('mouseover', (d) => {
         tooltip.style('display', 'block');
         title.text(d.label);
         sequence.text(d.seq);
@@ -440,39 +476,39 @@ function drawChart() {
         tooltip.attr('transform', `translate(${tooltipX}, ${tooltipY})`);
     });
     // Add label
-    tooltip = svg.append('g')
-        .style('display', 'none')
-        .attrs({
-        id: 'chart-tooltip'
+    tooltip = svg.append('g').style('display', 'none').attrs({
+        id: 'chart-tooltip',
     });
-    background = tooltip.append('rect')
-        .attrs({
+    background = tooltip.append('rect').attrs({
         width: '370px',
         height: '70px',
         fill: 'white',
         stroke: 'black',
-        rx: '5px'
+        rx: '5px',
     });
-    closeButton = tooltip.append('text').text('X').attrs({
+    closeButton = tooltip
+        .append('text')
+        .text('X')
+        .attrs({
         x: 355,
         y: 18,
-        'font-weight': 700
-    }).style('cursor', 'pointer')
-        .on('click', () => { tooltip.style('display', 'none'); });
-    title = tooltip.append('text').text('title')
-        .attrs({
-        x: 10,
-        y: 20
+        'font-weight': 700,
+    })
+        .style('cursor', 'pointer')
+        .on('click', () => {
+        tooltip.style('display', 'none');
     });
-    sequence = tooltip.append('text').text('sequence')
-        .attrs({
+    title = tooltip.append('text').text('title').attrs({
         x: 10,
-        y: 40
+        y: 20,
     });
-    gcContent = tooltip.append('text').text('gc Content')
-        .attrs({
+    sequence = tooltip.append('text').text('sequence').attrs({
         x: 10,
-        y: 60
+        y: 40,
+    });
+    gcContent = tooltip.append('text').text('gc Content').attrs({
+        x: 10,
+        y: 60,
     });
 }
 $('#advancedOptions input').change(() => {
